2562f41395f0eb650053f8d85bb9d1c73af9fb36
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-11-03 13:58:53 +0530

    blog added

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,5 +1,6 @@
 import Blog from  '../img/blogs/Blog.png';
 import dev from  '../img/blogs/dev.png';
+import tata from  '../img/blogs/tata.png';
 
 const blogs = [
     {
@@ -17,7 +18,16 @@ const blogs = [
         src:"https://agodaclone.hashnode.dev/building-clone-of-the-agoda-website",
         month: 'Oct 2021',
         image: dev
+    },
+    {
+        id: 3,
+        title: 'Building clone of the TataClick Website',
+        date: "03",
+        src:"https://hashnode.com/post/tatacliq-clone-ckvj97qae00cyazs1dr51b6xw",
+        month: 'Oct 2021',
+        image: tata
     }
+
 ]
 
 export default blogs;
